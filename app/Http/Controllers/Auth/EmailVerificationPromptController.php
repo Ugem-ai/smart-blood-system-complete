c456d6f2c81ff6bf79a5a5e338aa059771bc4cfd
Smart Blood Dev <dev@local.test>
2026-05-02 10:13:16 +0800

    fix  emailverificationpromptcontroller

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -15,7 +15,7 @@ class EmailVerificationPromptController extends Controller
     public function __invoke(Request $request): RedirectResponse|View
     {
         return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended('/')
-                    : view('app');
+                    ? redirect()->intended(route('dashboard', absolute: false))
+                    : view('auth.verify-email');
     }
 }
